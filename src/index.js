4554c6450632298b8221774c39f1634a38313ac1
LocS3R <locdev002@gmail.com>
2023-09-29 10:14:57 +0700

    install and setup handlebars template engine

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import express from "express";
 import morgan from "morgan";
+import { engine } from "express-handlebars";
+import { fileURLToPath } from "url";
+import path from "path";
+
 const app = express();
 const port = 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log("Directory Path:", __dirname);
+
 app.use(morgan("combined"));
 
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
+});
+
+app.get("/news", (req, res) => {
+  res.render("news");
 });
 
 app.listen(port, () =>
